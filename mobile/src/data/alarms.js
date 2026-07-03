25b7e00f9cf9340c9c0f460531f3e7d4ff29a2d6
Satti Keerthi Ratna Kousalya Devi <keerthiratna.satti@gmail.com>
2026-07-03 20:47:05 +0530

    feat(mobile): initialize Expo app with navigation and mock authentication

---
+++ mobile/src/data/alarms.js
@@ -0,0 +1,20 @@
+export const alarms = [
+  {
+    id: "1",
+    time: "06:00 AM",
+    label: "Morning Workout",
+    enabled: true,
+  },
+  {
+    id: "2",
+    time: "07:30 AM",
+    label: "College",
+    enabled: true,
+  },
+  {
+    id: "3",
+    time: "09:00 AM",
+    label: "Weekend Alarm",
+    enabled: false,
+  },
+];
---
+++ mobile/src/navigation/RootNavigator.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import LoginScreen from "../screens/LoginScreen";
+import TabNavigator from "./TabNavigator";
+
+const Stack = createNativeStackNavigator();
+
+export default function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ title: "Login" }}
+      />
+
+      <Stack.Screen
+        name="Main"
+        component={TabNavigator}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
